9a4264f07c59afc902dc8f1cd96daa8156385e72
egorkharaim <egor.kharaima@gmail.com>
2026-05-31 18:59:05 +0300

    Fix: add password encoding in register method

--- src/main/java/mate/academy/service/user/UserServiceImpl.java
+++ src/main/java/mate/academy/service/user/UserServiceImpl.java
@@ -11,6 +11,7 @@ import mate.academy.model.user.RoleName;
 import mate.academy.model.user.User;
 import mate.academy.repository.user.RoleRepository;
 import mate.academy.repository.user.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,15 +21,20 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(
             UserRegistrationRequestDto requestDto) throws RegistrationException {
+        if (userRepository.existsByEmail(requestDto.email())) {
+            throw new RegistrationException("This email: "
+                    + requestDto.email()
+                    + " is already taken");
+        }
+
         User user = userMapper.toModel(requestDto);
 
-        if (userRepository.existsByEmail(requestDto.email())) {
-            throw new RegistrationException("This email: " + user.getEmail() + " is already taken");
-        }
+        user.setPassword(passwordEncoder.encode(requestDto.password()));
 
         Role userRole = roleRepository.findByName(RoleName.USER)
                 .orElseThrow(() -> new RegistrationException("Can't find default role USER"));
